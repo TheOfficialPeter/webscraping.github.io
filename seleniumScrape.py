14ea24c696e4cfceb40874104009932d67aa5c6f
Peter <57006688+TheOfficialPeter@users.noreply.github.com>
2023-01-04 23:51:20 +0200

    Add files via upload

--- seleniumScrape.py
+++ seleniumScrape.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome("C:/chromedriver.exe")
-result = driver.get("https://www.youtube.com")
+result = driver.get("https://www.youtube.com/results?search_query=cats")
+
+videoTitles = driver.find_elements(By.ID, "video-title")
+
+
+
